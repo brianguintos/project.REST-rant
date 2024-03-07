ffa4a85e69be3adaaa9af1ca4cb00fa3e7cddeda
Brian Guintos Recinos <brianguintos@gmail.com>
2024-03-06 22:16:38 -0800

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
 //This is the Home route
 app.get ('/', (req, res) => {
-    res.send('eatmapuzzy!')
+    res.send('Hello world!')
 })
 
 app.listen(3000)
