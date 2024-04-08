a23d016a58d0c3c7ef29349fc74f355fb149bc39
Brian Guintos Recinos <brianguintos@gmail.com>
2024-04-07 19:13:34 -0700

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                    <img src="/images/error404pug.jpg" alt="404PUG" width="400" height="400" />
+                </div>
+              <div>
+                Photo by <a href="https://unsplash.com/@charlesdeluvio?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">charlesdeluvio</a> on <a href="https://unsplash.com/photos/black-dog-wearing-blue-denim-collar-K4mSJ7kc0As?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/steak-dinner.jpg" alt="Steak" width="400" height="400" />
+                </div>
+                <div>
+                Photo by <a href="https://unsplash.com/@sunorwind?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">sunorwind</a> on <a href="https://unsplash.com/photos/cooked-food-on-blue-ceramic-plate-Z4CvBOpOi6w?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                </div>
                 <a href="/places">
   <button className="btn-primary">Places Page</button>
 </a>
