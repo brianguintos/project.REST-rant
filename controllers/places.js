ce7f5bb85e5adf678acc7fac0edd15514bab6de6
Brian Guintos Recinos <brianguintos@gmail.com>
2024-04-08 23:11:17 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -5,6 +5,19 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+  res.render('places/show',{ place: places[id] })
+  }
+})
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
